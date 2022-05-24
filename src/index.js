1e81dfb6901033d08e5146237b18f48ffcf4db76
Shivam <thisisshivam06@gmail.com>
2022-05-24 19:33:03 +0530

    React Challenge 1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Shivam Singh Netflix Pick</h1>
+    <p>List of 5 Best Series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra Curricular</li>
+      <li>My Holo love</li>
+      <li>My first -2 love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
