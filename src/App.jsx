c2b07d6d29840c8c6a2f818b24a47d2c17071644
Shivam <thisisshivam06@gmail.com>
2022-05-27 15:52:11 +0530

    Netflix Project Final

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Cards";
+import Sdata from "./Sdata";
+
+const App = () => (
+  <>
+    <h1 className="heading_style"> List of top 5 Netflix Series in 2020 </h1>
+    {Sdata.map((val, index) => {
+      console.log(index);
+      return (
+        <Card
+          key={val.id}
+          imgsrc={val.imgscr}
+          title={val.title}
+          sname={val.sname}
+          link={val.links}
+        />
+      );
+    })}
+  </>
+);
+
+export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,49 @@
+const Sdata = [
+  {
+    id: 1,
+    sname: "DARK",
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 2,
+    sname: "extra Curricular",
+    imgscr:
+      "https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 3,
+    sname: "Stranger Things",
+    imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 4,
+    sname: "the Vampire Diaries",
+    imgscr:
+      "https://rukminim2.flixcart.com/image/416/416/krdtlzk0/poster/o/m/b/medium-the-vampire-diaries-poster-fantasy-all-newtvps416-original-imag56xaxrszw8g5.jpeg?q=70",
+    title: "azon Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 5,
+    sname: "My First-2 love",
+    imgscr:
+      "https://i0.wp.com/fzmovies.skipogist.com/wp-content/uploads/2021/06/68737604_659647781193309_8021678197894545408_n-1.jpg?fit=284%2C398&ssl=1",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 6,
+    sname: "extra Curricular",
+    imgscr:
+      "https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+];
+export default Sdata;
